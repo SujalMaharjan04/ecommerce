e1cc1bfa1d03d453145e9da0d734d8ef93d7c1d0
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-09-16 12:33:07 +0545

    Change the esewa integration from checkout to place_order

--- Ecommerce/process/place_order.php
+++ Ecommerce/process/place_order.php
@@ -15,11 +15,38 @@ $total_price = $_POST['total_price'];
 $product_ids = $_POST['product_id'];
 $quantities = $_POST['quantity'];
 
+// Esewa Integration
+//Configuration
+$secret = '8gBm/:&EnhH.1/q'; //test secret key-> esewa docs
+$product_code = 'EPAYTEST';//test code from esewa
+
+$failure = '###'; // add a failure page
+
+//Transaction Details
+$amount = $total_price;
+$tax_amount = '10';
+$total_amount = $amount+ $tax_amount;
+$transaction_uuid = uniqid('txn_');
+
+
+//prepare signed fields 
+$signed_field_names = 'total_amount,transaction_uuid,product_code';
+$date_to_sign = "total_amount=$total_amount,transaction_uuid=$transaction_uuid,product_code=$product_code";
+
+//hash the signature
+$signature = base64_encode(hash_hmac('sha256', $date_to_sign, $secret, true));
+
+
 // Insert order into database
 $stmt = $conn->prepare("INSERT INTO orders (customer_name, email, address, total_price, status) VALUES (?, ?, ?, ?, 'Pending')");
 $stmt->bind_param("sssd", $name, $email, $address, $total_price);
 $stmt->execute();
 $order_id = $stmt->insert_id;
+
+//Success Page
+// $success = 'https://localhost/ecommerce/order_success.php?order_id=' . $order_id;
+
+
 
 // Deduct stock and clear cart
 foreach ($product_ids as $index => $product_id) {
@@ -35,6 +62,26 @@ foreach ($product_ids as $index => $product_id) {
 unset($_SESSION['cart']);
 
 // Redirect to order success page
-header("Location: ../order_success.php?order_id=" . $order_id);
-exit();
+// header("Location: ../order_success.php?order_id=" . $order_id);
+// exit();
 ?>
+
+<body>
+    <form id = "esewa-form" action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST"> <!--Change the action to new url given by esewa for production -->
+        <input type="hidden" id="amount" name="amount" value="<?= $amount ?>" required>
+        <input type="hidden" id="tax_amount" name="tax_amount" value ="<?= $tax_amount ?>" required>
+        <input type="hidden" id="total_amount" name="total_amount" value="<?= $total_amount ?>" required>
+        <input type="hidden" id="transaction_uuid" name="transaction_uuid" value="<?= $transaction_uuid ?>" required>
+        <input type="hidden" id="product_code" name="product_code" value ="<?= $product_code ?>" required>
+        <input type="hidden" id="product_service_charge" name="product_service_charge" value="0" required>
+        <input type="hidden" id="product_delivery_charge" name="product_delivery_charge" value="0" required>
+        <input type="hidden" id="success_url" name="success_url" value="https://developer.esewa.com.np/success" required>
+        <input type="hidden" id="failure_url" name="failure_url" value="https://developer.esewa.com.np/failure" required>
+        <input type="hidden" id="signed_field_names" name="signed_field_names" value="total_amount,transaction_uuid,product_code" required>
+        <input type="hidden" id="signature" name="signature" value="<?= $signature ?>" required>
+    </form>
+
+    <script>
+        document.getElementById('esewa-form').submit()
+    </script>
+</body>
